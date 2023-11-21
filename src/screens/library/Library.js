6f49848d7a391dee41539f9ab1876137bfc4b262
Wyeben <yila.ben@yahoo.com>
2023-11-21 06:48:08 +0100

    added code

--- src/screens/library/Library.js
+++ src/screens/library/Library.js
@@ -3,32 +3,36 @@ import APIKit, { setClientToken } from '../../Spotify';
 import './Library.css';
 
 export default function Library() {
+  const [playlists, setPlaylists] = useState([]);
 
-  const [playlists, setPlaylists] = useState('');
   useEffect(() => {
-    // const accessToken = window.location.hash.substring(1).split('&')[0].split('=')[1];
-    const accessToken = JSON.stringify(localStorage.getItem("token"));
-    console.log('Access Token:', accessToken);
+    const accessToken = localStorage.getItem('token');
+    if (!accessToken) {
+      console.error('Access token not found');
+      return;
+    }
 
     setClientToken(accessToken);
-    APIKit.get('me/playlists').then(function(response) {
-    console.log('API Response:', response);
 
-    setPlaylists(response.data.items);
-    console.log("response --> ",response.data);
-    console.log(response.data.items);
-  })
-  .catch(function (error) {
-    console.error('API Request Error:', error);
-  });
-  },[]);
-  return( <div className='screen-container'>
-    <div className='library-body'>
-    {playlists?.map((playlist) => (
-      <div className='playlist-card' key={playlist.id}>{playlist.name}</div>
-    ))}
+    APIKit.get('me/playlists')
+      .then(function (response) {
+        console.log('API Response:', response);
+        setPlaylists(response.data.items || []);
+      })
+      .catch(function (error) {
+        console.error('API Request Error:', error);
+      });
+  }, []);
+
+  return (
+    <div className='screen-container'>
+      <div className='library-body'>
+        {playlists.map((playlist) => (
+          <div className='playlist-card' key={playlist.id}>
+            {playlist.name}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
-  
+  );
 }
